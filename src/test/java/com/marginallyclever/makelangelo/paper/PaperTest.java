a64d5767f93de61bae4a80efb6b8fc25c82ce69c
FaraDuMatin <118701688+FaraDuMatin@users.noreply.github.com>
2024-10-10 15:37:07 -0400

    Update PaperTest.java

--- src/test/java/com/marginallyclever/makelangelo/paper/PaperTest.java
+++ src/test/java/com/marginallyclever/makelangelo/paper/PaperTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
 public class PaperTest {
@@ -30,13 +31,15 @@ public class PaperTest {
 		// TODO: this is a potentially destructive change if the test fails.
 	}
 
+	//@Test(expected = [Erreur.Exception]
+
 	@Test
 	public void testPaperLocation() {
 		Paper a = new Paper();
 		a.setPaperSize(200,100,0,0);
 		a.setPaperMargin(0.9);
 
-		Rectangle2D.Double rect = a.getMarginRectangle();		
+		Rectangle2D.Double rect = a.getMarginRectangle();
 		Assertions.assertEquals(180,rect.getWidth());
 		Assertions.assertEquals(90,rect.getHeight());
 		Assertions.assertEquals(-90,rect.getMinX());
@@ -45,7 +48,7 @@ public class PaperTest {
 		Assertions.assertEquals(45,rect.getMaxY());
 		Assertions.assertEquals(0,a.getCenterX());
 		Assertions.assertEquals(0,a.getCenterY());
-		
+
 		a.setPaperSize(200,100,50,100);
 		rect = a.getMarginRectangle();
 		Assertions.assertEquals(180,rect.getWidth());
@@ -57,4 +60,33 @@ public class PaperTest {
 		Assertions.assertEquals(50,a.getCenterX());
 		Assertions.assertEquals(100,a.getCenterY());
 	}
+
+	@Test
+	public void testGetPaperWidth() {
+		Paper paper = new Paper(); // Create a new Paper object
+
+		// Default width should be 420 mm (as defined by DEFAULT_WIDTH)
+		double expectedWidth = 420;
+		Assertions.assertEquals(expectedWidth, paper.getPaperWidth(), 0.001, "The paper width should be 420mm.");
+
+		// Test after setting a new paper size
+		paper.setPaperSize(500, 700, 0, 0);
+		expectedWidth = 500;
+		Assertions.assertEquals(expectedWidth, paper.getPaperWidth(), 0.001, "The paper width should be 500mm.");
+	}
+
+	@Test
+	public void testGetPaperHeight() {
+		Paper paper = new Paper();
+		// Default height
+		double expectedHeight = Paper.DEFAULT_HEIGHT;
+		Assertions.assertEquals(expectedHeight, paper.getPaperHeight(), 0.001, "Default paper height should be " + expectedHeight + "mm.");
+
+		// After setting a new size
+		paper.setPaperSize(500, 700, 0, 0);
+		Assertions.assertEquals(700, paper.getPaperHeight(), 0.001, "Paper height should be 700mm.");
+	}
+
+
+
 }
